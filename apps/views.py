83fb37d90b0c40aff00c63e8e7a0123facb4c9dc
Anang <tyoandini@gmail.com>
2021-05-12 09:39:20 +0700

    templates - 2 (kirim variabel dan logic)

--- apps/views.py
+++ apps/views.py
@@ -3,7 +3,14 @@ from django.http import HttpResponse
 
 
 def komik(request):
-    return render(request, 'komik.html')
+    judul = "Belajar Django"
+    komik = ["mereka", "dia", "akan", "segera"]
+    data = {
+        'judul' : judul,
+        'penulis' : 'Anang Nur Prasetya',
+        'komik' : komik,
+    }
+    return render(request, 'komik.html', data)
 
 def penerbit(request):
     return HttpResponse('Ini adalah halaman penerbit')
