65a24bccb396a27100b91f9edf9608f30ff8bd43
Anang <tyoandini@gmail.com>
2021-05-17 14:36:15 +0700

    models - 2 foreign key

--- apps/models.py
+++ apps/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Negara(models.Model):
+	nama = models.CharField(max_length=50)
+	keterangan = models.TextField()
+
+	def __str__(self):
+		return self.nama
+
 class Komik(models.Model):
 	judul = models.CharField(max_length=50)
 	penulis = models.CharField(max_length=40)
 	penerbit = models.CharField(max_length=40)
 	jumlah = models.IntegerField(null=True)
+	id_negara = models.ForeignKey(Negara, on_delete = models.CASCADE, null=True)
 
 	def __str__(self):
 		return self.judul
