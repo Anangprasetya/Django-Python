346338930825b7abec6f4556e839c1d7a59efcea
Anang <tyoandini@gmail.com>
2021-05-18 07:25:18 +0700

    admin - 2 (mengatur tampilan admin model)

--- apps/admin.py
+++ apps/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from apps.models import *
 
-admin.site.register(Komik)
+class KomikAdmin(admin.ModelAdmin):
+	list_display = ('judul', 'penulis', 'id_negara', 'jumlah')
+	list_filter = ['id_negara']
+	search_fields = ['judul', 'penulis'] 
+	list_per_page = 2
+
+admin.site.register(Komik, KomikAdmin)
 admin.site.register(Negara)
 
 # Register your models here.
