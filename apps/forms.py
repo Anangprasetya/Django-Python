1f386d6c7535bdfb24d961da076d8f37db06bacd
Anang <tyoandini@gmail.com>
2021-05-22 18:16:13 +0700

    forms - widgets

--- apps/forms.py
+++ apps/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from apps.models import Komik
 
 
@@ -7,4 +8,14 @@ class KomikForm(ModelForm):
         model = Komik
         fields = '__all__'
         # exclude = [''] kecuali 
+
+        widgets = {
+            'judul' : forms.TextInput({'class':'form-control'}),
+            'penulis' : forms.TextInput({'class':'form-control'}),
+            'penerbit' : forms.TextInput({'class':'form-control'}),
+            'jumlah' : forms.NumberInput({'class':'form-control'}),
+            'id_negara' : forms.Select({'class':'form-control'}),
+        }
+
+
     
